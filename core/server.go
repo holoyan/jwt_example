3b6a25cf0048fd1b3a8f59ab1a588f179c8b9c05
Artak <artaksafaryanc@gmail.com>
2023-08-19 16:43:18 +0400

    add simple http server

--- core/server.go
+++ core/server.go
@@ -3,12 +3,22 @@ package core
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 func Run()  {
-	port := 8082
-	fmt.Printf("Server started on port %d\n", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	var port = os.Getenv("PORT")
+	// string to int
+	intPort, error := strconv.Atoi(port)
+	if error != nil {
+		// ... handle error
+		intPort = 8085
+	}
+
+	fmt.Println()
+	fmt.Printf("Server started on port %d\n", intPort)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", intPort), nil)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
--- main.go
+++ main.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	route.Register()
-
+	core.Load()
 	core.Run()
 }
